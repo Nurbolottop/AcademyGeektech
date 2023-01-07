1f6e6add31e019225a200813c626cd87e156af39
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-01-07 14:49:52 +0600

    first

--- apps/settings/models.py
+++ apps/settings/models.py
@@ -23,15 +23,25 @@ class Settins(models.Model):
         verbose_name_plural = "Настройки"
 
 class Slides(models.Model):
-    slide_image = models.ImageField(
+    slide_image1 = models.ImageField(
         upload_to="slide_image",
-        verbose_name="Фотография слайда"
+        verbose_name="Фотография первого  слайда"
     )
-    name_slide = models.TextField(
-        verbose_name="Название  слайда"
+    name_slide1 = models.TextField(
+        verbose_name="Название  первого  слайда"
     )
-    description_slide = models.TextField(
-        verbose_name="Описание слайда"
+    description_slide1 = models.TextField(
+        verbose_name="Описание  первого слайда"
+    )
+    slide_image2 = models.ImageField(
+        upload_to="slide_image",
+        verbose_name="Фотография второго слайда"
+    )
+    name_slide2 = models.TextField(
+        verbose_name="Название  второго слайда"
+    )
+    description_slide2 = models.TextField(
+        verbose_name="Описание второго слайда"
     )
     def __str__(self):
         return self.name_slide
