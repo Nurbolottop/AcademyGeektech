08f41740d4335aefa7e261f757442bf82701776c
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-01-09 19:51:31 +0600

    Teacher.html

--- apps/about/models.py
+++ apps/about/models.py
@@ -19,6 +19,7 @@ class Teams(models.Model):
         upload_to="techer_image/",
         verbose_name="Фотография учителя"
     )
+
     name = models.CharField(
         max_length=255,
         verbose_name="Имя учителя"
@@ -36,6 +37,22 @@ class Teams(models.Model):
         max_length=255,
         verbose_name="Почта учителя"
         )
+    faceebook = models.URLField(
+        verbose_name="Facebook",
+        blank=True, null=True
+    )
+    instagram = models.URLField(
+        verbose_name="Instagram",
+        blank=True, null=True
+    )
+    telegram = models.URLField(
+        verbose_name="Telegram",
+        blank=True, null=True
+    )
+    whatsapp = models.URLField(
+        verbose_name="WhatsApp",
+        blank=True, null=True
+    )
 
     def __str__(self):
         return self.name
